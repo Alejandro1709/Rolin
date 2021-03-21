dd95257f1d5d4ec70edad548e5975e3cd90aa07f
Alejandro <alejandro.lr.developer@hotmail.com>
2021-03-21 12:52:03 -0500

    Model Done

---
+++ models/User.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  fullName: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  username: {
+    type: String,
+    required: true,
+    trim: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    trim: true,
+    min: 6,
+  },
+});
+
+module.exports = mongoose.model('User', userSchema);
